d6fc674acb0a50653f01792180619698814d29a5
Kavya Shah <kavyaatulshahpatan@gmail.com>
2025-02-11 22:23:57 +0530

    golang-cors

--- golang-service/api/main.go
+++ golang-service/api/main.go
@@ -11,15 +11,31 @@ import (
 	"golang-service/api/handlers"
 	"golang-service/api/models"
 	"golang-service/api/utils"
+	"github.com/gin-contrib/cors"
+
 )
 
 func InitializeRouter() *gin.Engine {
 	router := gin.Default()
+
+	// Custom CORS configuration
+	config := cors.Config{
+		AllowOrigins:     []string{"https://cred-vault.vercel.app/", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}
+
+	router.Use(cors.New(config))
+
 	router.POST("/api/send-code", handlers.SendCode)
 	router.POST("/api/verify-code", handlers.VerifyCode)
 	router.GET("/api/health", handlers.HealthCheck)
+
 	return router
 }
+
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	if err := godotenv.Load(); err != nil {
